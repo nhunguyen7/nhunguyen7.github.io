cd7c154e65183cd11722a9d0b3e533098739c723
nhunguyen7 <91999136+nhunguyen7@users.noreply.github.com>
2024-01-04 15:40:29 -0500

    Update website

--- script2.js
+++ script2.js
@@ -1,15 +1,3 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var pageUrl = window.location.pathname;
-  var homeHeading = document.getElementById('home-btn');
-  var aboutHeading = document.getElementById('about-btn');
-  
-  if (pageUrl.includes('index.html')) {
-    homeHeading.classList.add('bold');
-  } else if (pageUrl.includes('about.html')) {
-    aboutHeading.classList.add('bold');
-  }
-});
-  
 document.addEventListener('DOMContentLoaded', function() {
   var navbarToggle = document.querySelector('.navbar-toggle');
   var navBar = document.querySelector('.nav-bar');
